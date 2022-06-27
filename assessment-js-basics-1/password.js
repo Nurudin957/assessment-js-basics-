467980ed3c7e17f15f3bd80b9b1a27f62493f2cc
Haykal Ahmed <107882261+Nurudin957@users.noreply.github.com>
2022-06-27 02:50:35 -0400

    Assesement completed

---
+++ assessment-js-basics-1/password.js
@@ -0,0 +1,8 @@
+
+const password = "haykalAhmed";
+
+if (password > 10) {
+    console.log('success');
+}else{
+    console.log('the password diddnt`t match the rules');
+}
---
+++ assessment-js-basics-1/sdlc.js
@@ -0,0 +1,35 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const Analysis_of_Requirements = 'This is second step where in which the end goal is to decide whether the requirements can be achieved or not';
+const Design = 'This is the third step where the designing of the software takes place';
+const Implementation = 'This is the fourth step in which the system is developed by the developer , written in the langauage or framework choosen by the developers to be suitable to system requirements';
+const Testing_and_Integration = 'This is the step in which the system is tested for any erros or bugs that may occur or has already occured';
+const Maintain = 'This is the final step in which it always takes place , its where the system is maintained for any future breaks or any updates its deal with';
